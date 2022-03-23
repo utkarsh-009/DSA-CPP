920624bcba35603e2ad00d7062fd8de143a4e741
utkarsh-009 <utkarshpatil321@gmail.com>
2022-03-23 12:19:08 +0530

    changes done

---
+++ Linked Lists/LLTraversal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Creating Node Structure
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// With pointer of head, we can traverse entire Linked List
+void printLL(Node *head) // Giving head as parameter
+{
+    Node *curr = head;   // Creating a temp pointer for traversal
+    while (curr != NULL) // NOTE: In condition, curr.next != null will not print last last node
+    {
+        cout << curr->data << " "; // Printing the data of curr node
+        curr = curr->next;         // For jumping to next node
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    printLL(head);
+}
---
+++ Linked Lists/LLTraversal_recur.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void recur_LLprint(Node *head)
+{
+    if (head == NULL) // If head is equal to null, return void
+    {
+        return;
+    }
+    cout << head->data << " "; // Else return data
+    recur_LLprint(head->next); // Performing Print Linked List function recursively on next node i.e. head.next
+}
+
+int main()
+{
+    Node *head = new Node(50);
+    head->next = new Node(40);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(20);
+    head->next->next->next->next = new Node(10);
+
+    recur_LLprint(head);
+}
