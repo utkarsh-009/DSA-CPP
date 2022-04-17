2723798b49e770b72075328785d6556ac19faa8c
utkarsh-009 <utkarshpatil321@gmail.com>
2022-04-17 11:54:31 +0530

    comments added

--- Searching/iter_binarySearch.cpp
+++ Searching/iter_binarySearch.cpp
@@ -1,23 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int binSearch(int arr[], int n, int x)
 {
     int low = 0, high = n - 1;
-
+    // After every iteration, the length between high and low will decrease.
+    // At some point, when given element is not to be found then the low will be either greater or equal to the high. This is where we break out of loop
     while (low <= high)
     {
-        int mid = low + (high - low) / 2;
+        int mid = low + (high - low) / 2; // To avoid overflow
         if (arr[mid] == x)
         {
-            return mid;
+            return mid; // if element is found, return its index
         }
         else if (arr[mid] > x)
         {
+            // if mid element of array is greater than x(the integer to be searched),then decrease high to mid - 1 i.e. high = mid - 1;
             high = mid - 1;
         }
         else
         {
+            // if mid element of array is smaller than x(the integer to be searched), then increase low to mid + 1 i.e. low = mid + 1;
             low = mid + 1;
         }
     }
