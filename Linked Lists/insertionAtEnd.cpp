596c3cdf59aea8153486c07f64974282a18888c4
utkarsh-009 <utkarshpatil321@gmail.com>
2022-07-21 10:21:39 +0530

    Inserting Node at End of LL

---
+++ Linked Lists/insertionAtEnd.cpp
@@ -0,0 +1,61 @@
+/*
+
+I/P: 10->20->30->NULL, x = 5
+O/P: 10->20->30->5->NULL
+
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *insertAtEnd(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+    {
+        return temp;
+    }
+
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+
+    curr->next = temp;
+    return head;
+}
+
+// To print Linked List
+void printLL(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    head = insertAtEnd(head, 5);
+
+    printLL(head);
+}
