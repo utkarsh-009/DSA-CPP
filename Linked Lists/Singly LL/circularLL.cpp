fba76fba3443d287c2197cfd54ab824213148dbd
utkarsh-009 <utkarshpatil321@gmail.com>
2022-07-22 09:52:53 +0530

    Done

---
+++ Linked Lists/Singly LL/circularLL.cpp
@@ -0,0 +1,44 @@
+/*
+
+10's next is 30 (i.e tail is connected to head)
+30->20->10
+ ^------'
+*/
+
+#include <iostream>
+using namespace std;
+
+// Creating structure for Node
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// To print Linked List
+void printLL(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = head;
+
+    printLL(head); // Infinite loop
+}
