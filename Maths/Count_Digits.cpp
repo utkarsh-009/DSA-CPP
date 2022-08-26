7e66ae8b7a524caec69948cd44f5d5dab0e66e25
utkarsh-009 <utkarshpatil321@gmail.com>
2022-08-26 12:20:17 +0530

    Count Number of Digits: Constant Time Solution IMP

---
+++ Maths/Count_Digits.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Recursive [TC: O(No of Digits), AS: O(No of Digits)]
+int countDigits(long n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    return 1 + countDigits(n / 10);
+}
+
+// Iterative: [TC: O(No of Digits), AS: O(1)]
+int countDigits(long n)
+{
+    int cnt = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        cnt++;
+    }
+    return cnt;
+}
+
+// Logarithmic [TC: O(1), AS: O(1)]
+int countDigits(long n)
+{
+    return floor(log10(n)) + 1;
+}
