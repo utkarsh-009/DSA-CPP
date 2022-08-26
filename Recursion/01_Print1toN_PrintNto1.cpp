bedbde7649180ab48991c5d8aecbe67266fec8a2
utkarsh-009 <utkarshpatil321@gmail.com>
2022-08-26 22:15:16 +0530

    Print 1 to N and N to 1 using Recursion

---
+++ Recursion/01_Print1toN_PrintNto1.cpp
@@ -0,0 +1,40 @@
+/*
+    I/P: 4
+    O/P: 1 2 3 4
+         4 3 2 1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  1 to N [TC: O(n),AS: O(n)] (Tail Recursion)
+void print1toN(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    print1toN(n - 1);
+    cout << n << " ";
+}
+
+// N to 1 [TC: O(n),AS: O(n)] (No Tail Recursion)
+void printNto1(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    cout << n << " ";
+    print1toN(n - 1);
+}
+
+int main()
+{
+    int n = 4;
+    print1toN(n);
+    cout << endl;
+    printNto1(n);
+}
