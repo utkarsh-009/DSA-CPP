ecc48232dbc4f6da941c8c62d8c0d0874f209d9f
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-05 00:05:35 +0530

    Maximum Circular Sum Subarray: Circular Subarray Sum Implementation IMP

--- Arrays/19_Max_Circular_Sum_Subarray.cpp
+++ Arrays/19_Max_Circular_Sum_Subarray.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 //  [TC: O(n), AS: O(1)]
-int maxSubarraySumDivAndCon(int arr[], int n)
+int maxNormalSubarraySum(int arr[], int n)
 {
     int maxSS = 0, sum = 0;
 
@@ -30,11 +30,32 @@ int maxSubarraySumDivAndCon(int arr[], int n)
     return maxSS;
 }
 
+int maxSubarraySum(int arr[], int n)
+{
+    int max_normal = maxNormalSubarraySum(arr, n); // calculating maximum subarray sum using kadane's algorithm
 
+    // it means all elements are less than 0 which implies max normal will be equal to the smallest negative number
+    if (max_normal < 0)
+    {
+        return max_normal;
+    }
+
+    int arr_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        arr_sum += arr[i]; // Total Array Sum
+        arr[i] = -arr[i];  // Inversion of array
+    }
+
+    // After inverting array maxNormalSubarraySum(arr, n) will give minimum subarray sum
+    int max_circular = arr_sum + maxNormalSubarraySum(arr, n); // Instead of subtracting, we will add minimum subarray sum as array is inverted
+
+    return max(max_normal, max_circular);
+}
 
 int main()
 {
-    int arr[] = {8, -4, 3, -5, 4}, n = 5;
+    int arr[] = {5, -2, 3, 4}, n = 4;
 
-    cout << maxCircularSumSubarray(arr, n);
+    cout << maxSubarraySum(arr, n);
 }
