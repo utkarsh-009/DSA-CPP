d5070ca6d3142431f5cb9cfe1f02e6013f95b20a
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-08 23:28:05 +0530

    Counting Inversion of Unsorted Array: Efficient Using Merge Sort

--- Sorting/11_Counting_Inversion_Efficient.cpp
+++ Sorting/11_Counting_Inversion_Efficient.cpp
@@ -1,7 +1,20 @@
+/*
+    Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+    Output: 3
+    Explanation: The sequence 2, 4, 1, 3, 5
+    has three inversions (2, 1), (4, 1), (4, 3).
+
+    Input: N = 5, arr[] = {2, 3, 4, 5, 6}
+    Output: 0
+    Explanation: As the sequence is already
+    sorted so there is no inversion count.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int countAndMerge(int arr[], int l, int m, int r)
+// [TC: O(nlogn), AS: O(n)]
+int countAndMerge(int arr[], int l, int m, int r) // Simillar to merge function
 {
     int n1 = m - l + 1, n2 = r - m;
     int left[n1], right[n2];
@@ -14,21 +27,21 @@ int countAndMerge(int arr[], int l, int m, int r)
         right[j] = arr[m + 1 + j];
     }
 
-    int res = 0, i = 0, j = 0, k = l;
+    int res = 0, i = 0, j = 0, k = l; // res is total no of inversions
     while (i < n1 && j < n2)
     {
-        if (left[i] <= right[i])
+        if (left[i] <= right[i]) // No inversion
         {
             arr[k] = left[i];
             i++;
             k++;
         }
-        else
+        else // When inversion is possible
         {
             arr[k] = right[j];
             j++;
             k++;
-            res = res + (n1 - i);
+            res = res + (n1 - i); // Calculating total inversion for left[i] > right[i]
         }
     }
     while (i < n1)
@@ -46,15 +59,15 @@ int countAndMerge(int arr[], int l, int m, int r)
     return res;
 }
 
-int effCountInversion(int arr[], int l, int r)
+int effCountInversion(int arr[], int l, int r) // Simillar to mergeSort function
 {
     int res = 0;
     if (l < r)
     {
         int m = (r + l) / 2;
-        res += effCountInversion(arr, l, m);
-        res += effCountInversion(arr, m + 1, r);
-        res += countAndMerge(arr, l, m, r);
+        res += effCountInversion(arr, l, m);     // Recursively counting inversion and sorting left sub array
+        res += effCountInversion(arr, m + 1, r); // Recursively counting inversion and sorting right sub array
+        res += countAndMerge(arr, l, m, r);      // Recursively counting inversion and merging left and right subarray
     }
     return res;
 }
