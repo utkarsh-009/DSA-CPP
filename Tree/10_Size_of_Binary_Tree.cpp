cee16a3aca0ccbd5db1552a0b9c31e2536a03896
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-13 15:25:35 +0530

    Finding Total No of Node in Tree: Recur & Iter Solution

---
+++ Tree/10_Size_of_Binary_Tree.cpp
@@ -0,0 +1,79 @@
+/*
+  10
+ / \
+5  20
+   / \
+  30  6
+
+O/P: 5
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *right;
+    Node *left;
+
+    Node(int k)
+    {
+        key = k;
+        right = left = NULL;
+    }
+};
+
+// M1: Recursive [TC: O(n), AS: O(h)]
+int getSizeRecur(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    return getSizeRecur(root->left) + getSizeRecur(root->right) + 1;
+}
+
+// Irerative: Using Level Order Traversal [TC: O(n), AS: O(w)]
+int getSizeIter(Node *root)
+{
+    int size = 0;
+    if (root == NULL)
+    {
+        return size;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (q.empty() == false)
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        size++;
+
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+
+    return size;
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(20);
+    root->right->left = new Node(30);
+    root->right->right = new Node(6);
+
+    cout << getSizeIter(root);
+}
