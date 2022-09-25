aa8961a83e4d14bd178d46ca27e1b1e84a82bebf
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-25 18:31:45 +0530

    Fixing Binary Heap Having One Possible Violation

--- Heaps/02_Min_Heapify.cpp
+++ Heaps/02_Min_Heapify.cpp
@@ -5,24 +5,35 @@ Given a Binary Heap with one possible violation, fix the heap
 #include <bits/stdc++.h>
 using namespace std;
 
-//  [TC: ,AS:]
 class MinHeap
 {
     int *arr;
     int size, capacity;
 
 public:
-    int left(i)
+    MinHeap(int c)
+    {
+        arr = new int[c];
+        capacity = c;
+        size = 0;
+    }
+
+    int getElem(int i)
+    {
+        return arr[i];
+    }
+
+    int left(int i)
     {
         return 2 * i + 1;
     }
 
-    int right(i)
+    int right(int i)
     {
         return 2 * i + 2;
     }
 
-    int parent(i)
+    int parent(int i)
     {
         return (i - 1) / 2;
     }
@@ -43,8 +54,45 @@ public:
             i = parent(i);
         }
     }
+
+    /*
+    Intution: We pass index of node to minHeapify function, which maybe violating min heap property
+    This function fixes the subtree under the given node by making sure that root of the subtree has minimum value
+    */
+    // Recursive [TC: O(n), AS: O(h)] NOTE: Iterative Soln takes AS: O(1)
+    void minHeapifyRecur(int i) // Parent Index given as parameter
+    {
+        // Finding the indexes of left and right child
+        int lIdx = left(i), rIdx = right(i);
+        int smallest = i;
+
+        // Checking if left child index actually exist (lIdx < size) and then checking if root is smaller than left child
+        if (lIdx < size && arr[lIdx] < arr[i])
+        {
+            smallest = lIdx; // Update the smallest index
+        }
+
+        // Checking if right child index actually exist (rIdx < size) and then checking if right child is smallest
+        if (rIdx < size && arr[rIdx] < arr[smallest])
+        {
+            smallest = rIdx;
+        }
+
+        if (smallest != i) // If root is not the smallest amongst root, left child, right child
+        {
+            swap(arr[i], arr[smallest]); // Swap smallest with root
+            minHeapifyRecur(smallest);   // Recursievly Calling minHeapify for smallest index
+        }
+    }
 };
 
 int main()
 {
+    MinHeap h(5);
+    h.insert(3);
+    h.insert(24);
+    h.insert(15);
+    h.insert(20);
+
+    return 0;
 }
