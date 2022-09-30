72b987f403073f752808bb699d046796169ed50c
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-30 16:38:56 +0530

    DFS Traversal for Disconnected Graph When Source Vertex Not Given

---
+++ Graph/04_2_DFS_Undirected_Graph_Source_Not_Given.cpp
@@ -0,0 +1,61 @@
+/*
+Given an undirected graph and source vertex not given ,print D.F.S.
+Note: Graph May be Disconnected
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void DFSRec(vector<int> adj[], int s, bool visited[])
+{
+    visited[s] = true;
+    cout << s << " ";
+
+    for (int u : adj[s])
+    {
+        if (visited[u] == false)
+        {
+            DFSRec(adj, u, visited);
+        }
+    }
+}
+
+// DFS when source is not given and graph nodes are disconnected [TC: O(V+E)]
+// Intution: When Graph Nodes are disconnected then we will call DFS for every non-visited node.
+void DFS(vector<int> adj[], int V)
+{
+    bool visited[V];
+    for (int i = 0; i < V; i++)
+    {
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < V; i++)
+    {
+        if (visited[i] == false)
+        {
+            DFSRec(adj, i, visited);
+        }
+    }
+}
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+int main()
+{
+    int V = 5;
+    vector<int> adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 3, 4);
+
+    cout << "Following is Depth First Traversal for disconnected graphs: " << endl;
+    DFS(adj, V);
+
+    return 0;
+}
