5d1d76d8f05e2fd5dd52da43acc8d52e155291c9
utkarsh-009 <utkarshpatil321@gmail.com>
2023-01-09 15:14:17 +0530

    Implementation of Inorder Traversal: Recursive & Iterative

--- Tree/02_Inorder_Tree_Traversal.cpp
+++ Tree/02_Inorder_Tree_Traversal.cpp
@@ -12,7 +12,7 @@ Inorder Traversal: 5 10 30 20 6
 TC: O(n), AS: O(h)
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Node
@@ -29,13 +29,44 @@ struct Node
     }
 };
 
-void inorderTrav(Node *root)
+void inorderRecur(Node *root)
 {
     if (root != NULL)
     {
-        inorderTrav(root->left);
+        inorderRecur(root->left);
         cout << (root->key) << " ";
-        inorderTrav(root->right);
+        inorderRecur(root->right);
+    }
+}
+
+void inorderIter(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    stack<Node *> st;
+    Node *curr = root;
+
+    // Run the loop until (curr is not null or stack is not empty)
+    while (curr != NULL || st.empty() == false)
+    {
+        // Reaching to the left most Node of the curr Node
+        while (curr != NULL)
+        {
+            st.push(curr);
+            curr = curr->left;
+        }
+
+        // Now as curr is NULL and at top of stack we have unvisited leftmost node
+        curr = st.top(); // As curr is NULL, we will assign the unvisited leftmost node to it
+        st.pop();
+
+        cout << curr->key << " ";
+
+        // As we have visited the node and its left subtree. Now, it's right subtree's turn
+        curr = curr->right;
     }
 }
 
@@ -47,5 +78,7 @@ int main()
     root->right->left = new Node(30);
     root->right->right = new Node(6);
 
-    inorderTrav(root);
+    inorderRecur(root);
+    cout << endl;
+    inorderIter(root);
 }
