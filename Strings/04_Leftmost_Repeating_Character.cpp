095c87355ff4d2b7d6c1d6edf0e96aa060244512
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-02 14:57:11 +0530

    Leftmost Repeating Character

---
+++ Strings/04_Leftmost_Repeating_Character.cpp
@@ -0,0 +1,37 @@
+/*
+The leftmost repeating character is the character which is the first character that repeats itself in the string.
+    I/P:
+    O/P:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  [TC: ,AS:]
+int leftmostRepeating(string &s)
+{
+    vector<bool> visi(256, false);
+    int res = -1;
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (visi[s[i]])
+        {
+            res = i;
+        }
+        else
+        {
+            visi[s[i]] = true;
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    string str = "geeksforgeeks";
+    cout << "Index of leftmost repeating character:" << endl;
+    cout << leftmostRepeating(str) << endl;
+
+    return 0;
+}
