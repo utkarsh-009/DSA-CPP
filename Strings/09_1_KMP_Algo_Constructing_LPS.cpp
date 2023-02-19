eea6182e8e61fc1c1f2a0c3f309b10dd046c8f26
utkarsh-009 <utkarshpatil321@gmail.com>
2023-02-19 15:26:13 +0530

    Constructing LPS Array Using KMP Algorithm

--- Strings/09_1_KMP_Algo_Constructing_LPS.cpp
+++ Strings/09_1_KMP_Algo_Constructing_LPS.cpp
@@ -6,11 +6,16 @@ Ex: "abcd"
     Suffix:
     "", "d", "cd", "bcd", "abcd"
 
+    We have to store max length of common longest proper prefix and suffix array
+
 I/P: str = "ababc"
 O/P: lps[] = {0,0,1,2,0}
 
 I/P: str = "aaaa"
 O/P: lps[] = {0,1,2,3}
+
+I/P: str = "abacabad"
+O/P: lps[] = {0,0,1,0,1,2,3,0}
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -21,9 +26,9 @@ int longestProperPrefixSuffix(string str, int n)
     for (int len = n - 1; len > 0; len--) // Starting from max possible ans
     {
         bool flag = true;
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < len; i++) // checking if the current length is lps
         {
-            if (str[i] != str[n - len + i]) // not
+            if (str[i] != str[n - len + i]) // not equal
             {
                 flag = false;
                 break;
@@ -46,11 +51,56 @@ void fillLPS(string str, int lps[])
     }
 }
 
+// Efficient [TC: O(n)]
+void fillLPSEfficient(string str, int *lps)
+{
+    int n = str.length();
+    int len = 0;
+    int i = 1;
+    lps[0] = 0;
+
+    while (i < n)
+    {
+        /*
+        KMP Algorithm:
+        Case1: if len = lps[i-1] and str[i] == str[len] => lps[i] = len+1;
+        Case2: if str[i] != str[len] :
+                a.) if len == 0 => lps[i] = 0
+                b.) Else, we recursively apply
+                     len = lps[len-1] and compare str[i] with str[len]
+        */
+
+        // Case1
+        if (str[i] == str[len])
+        {
+            lps[i] = len + 1;
+            len++;
+            i++;
+        }
+
+        // Case2
+        else
+        {
+            // Case2a
+            if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+            // Case2b
+            else
+            {
+                len = lps[len - 1];
+            }
+        }
+    }
+}
+
 int main()
 {
     string txt = "abacabad";
     int lps[txt.length()];
-    fillLPS(txt, lps);
+    fillLPSEfficient(txt, lps);
     for (int i = 0; i < txt.length(); i++)
     {
         cout << lps[i] << " ";
