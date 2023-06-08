1a034ac8cc6a8cc10bdc1cc2104577d81bcd4bba
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-08 09:14:43 +0530

    Printing all Permutations of given string

---
+++ Recursion/10_Printing_All_Permutations.cpp
@@ -0,0 +1,36 @@
+/*
+    I/P: "abc"
+    O/P: abc acb bac bca cab cba
+
+    I/P: "ab"
+    O/P: ab ba
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//  [TC: O((time to print one permutaion)*(no of permutations)) => O(n * n!), AS: O(r-l)]
+void printAllPerm(string &s, int l, int r)
+{
+    if (l == r)
+    {
+        cout << s << " ";
+        return;
+    }
+
+    for (int i = l; i <= r; i++)
+    {
+        swap(s[l], s[i]);
+        printAllPerm(s, l + 1, r);
+        swap(s[l], s[i]);
+    }
+}
+
+int main()
+{
+    string s = "abc";
+    int n = s.length();
+
+    printAllPerm(s, 0, n - 1);
+    return 0;
+}
