c4bd09c551f01812ed32f7826ee9936140cf0400
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-14 12:41:45 +0530

    Implementing K Stacks in an array

--- Stack/05_Implement_K_Stacks_In_Array.cpp
+++ Stack/05_Implement_K_Stacks_In_Array.cpp
@@ -10,11 +10,18 @@ using namespace std;
 Intution:
 ->The idea is to use two extra arrays for efficient implementation of k stacks in an array.
     Extra Arrays:
-    1) top[]: This is of size k and stores indexes of top elements in all stacks.
-    2) next[]: This is of size n and stores indexes of next item for the items in array arr[].
+    1) top[]: This is of size k and stores current indexes of top elements in all stacks.
+    2) next[]: This is of size n and stores indexes of next items for each element in original array arr[].
+    NOTE: next[] also helps us to find next free available spot in the array.
+->Variables
+    1.) k => No of stacks implemented in array
+    2.) free_top => next available free slot in the original array
 -> push(int x, int sn) // pushing element x into stack no sn
     1.) Push the element into => arr[freeTop]
     2.) The next free spot => next[free]
+-> INITIALISATION:
+    top[] => all to -1
+    next[] => all i to i+1 except last index which is initialised as -1
 
     // CODE //
     int i = freeTop; // Store index of next free slot
@@ -25,6 +32,14 @@ Intution:
     // Put the element into array
     arr[i] = x;
 
+-> next[]: - This is of size n and stores indexes of next items for each element in original array arr[]
+    - next[] helps us to find next free available spot in the array
+    - next[] also helps to link the other elements of stack with top.
+    - if next[i] = -1 => arr[i] is the last element of stack
+    - next free spot is stored in variable next[free]
+    NOTE:
+    1.) When ith position is empty (next free index to push) => it stores next position of top when the element is pushed
+    2.) When ith position is filled (next top index after pop) => it stores next position of top if the element is popped
 */
 
 struct KStacks
@@ -69,10 +84,20 @@ struct KStacks
             return;
         }
 
+        // Store the index of current free top
         int i = freeTop;
+
+        // Update the next free top using next[i]
         freeTop = next[i];
+
+        // Storing next[i] as top[sn] which will help to link new element to whatever that is present in the stack
+        // This will help us, when ith element is popped => we get the next element using next[i]
         next[i] = top[sn];
+
+        // Updating the current top of the given stack number. The new top will be set to current free top.
         top[sn] = i;
+
+        // Update the element at current free top
         arr[i] = x;
     }
 
@@ -84,10 +109,19 @@ struct KStacks
             return INT_MAX;
         }
 
-        int i = top[sn];
+        // Element i.e. to be popped will be at current top of the stack.
+        // Using top[sn], we get the top index of given stack no sn.
+        int i = top[sn]; // stores current top index
+
+        // The next top index after pop operation will be updated in top[sn] using next[i]
         top[sn] = next[i];
+
+        // The next free index at ith position will be updated using freeTop
         next[i] = freeTop;
+
+        // The free top is also updated as after pop operation is performed, the current top index will be freeTop
         freeTop = i;
+
         return arr[i];
     }
 };
