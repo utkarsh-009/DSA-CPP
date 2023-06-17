37b22363beb0156f5a02f432dd1c11f4347abb19
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-17 17:33:37 +0530

    Middle of linked list

---
+++ Linked Lists/Singly LL/14_Middle_of_LL.cpp
@@ -0,0 +1,60 @@
+/*
+Input: 1->2->3->4->5
+Output: 3
+
+Input: 2->4->6->7->5->1
+Output: 7
+*/
+
+#include <iostream>
+using namespace std;
+
+// Creating structure for Node
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// O(n): Using slow and fast pointer
+int getMiddle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow->data;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    printList(head);
+    cout << "\nMiddle of Linked List: " << getMiddle(head);
+    return 0;
+}
