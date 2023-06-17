a0e619cba350d5201c99aa48d7b1416a92edb133
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-17 10:56:27 +0530

    Delete At Begin DLL

---
+++ Linked Lists/Doubly LL/delAtBeginDLL.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+Node *delHeadDLL(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+
+    Node *temp = head;
+    head = head->next;
+    head->prev = NULL;
+    delete temp;
+
+    return head;
+}
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->prev = head;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+    head = delHeadDLL(head);
+    printlist(head);
+    return 0;
+}
