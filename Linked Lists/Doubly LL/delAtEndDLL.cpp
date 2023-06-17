55180aa8b3b63cea0341b56d75c9cb81cabfabe2
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-17 11:01:14 +0530

    Delete Last of a Doubly Linked List

---
+++ Linked Lists/Doubly LL/delAtEndDLL.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node *delEndDLL(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+
+    Node *curr = head;
+    while (curr->next->next != NULL)
+    {
+        curr = curr->next;
+    }
+
+    Node *temp = curr->next;
+    curr->next = NULL;
+    delete temp;
+
+    return head;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->prev = head;
+    temp1->next = temp2;
+    temp2->prev = temp1;
+    head = delEndDLL(head);
+    printlist(head);
+    return 0;
+}
