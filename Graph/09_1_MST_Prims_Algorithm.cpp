7d998a678ee26c63e8241e34f1902594f65c9211
utkarsh-009 <utkarshpatil321@gmail.com>
2023-06-24 13:15:32 +0530

    Minimum Spanning Tree Using Prims Algorithm: O(V*V)

--- Graph/09_1_MST_Prims_Algorithm.cpp
+++ Graph/09_1_MST_Prims_Algorithm.cpp
@@ -27,10 +27,10 @@ using namespace std;
 
 /*
 Prim's Algorithm: To find MST.
-- Use a boolean array mstSet[] to represent the set of vertices included in MST.
-  If a value mstSet[v] is true, then vertex v is included in MST, otherwise not.
-- Array key[] is used to store key values of all vertices. Another array parent[] to store indexes of parent nodes in MST.
-- The parent array is the output array, which is used to show the constructed MST.
+Intuition: The algorithm starts from a given vertex and greedily expands the tree by adding the minimum weight edge at each step.
+- Use a boolean array mstSet[] to track the inclusion of vertices in the MST.
+- Array key[] is used to store and keep track of the minimum weight edge connecting each vertex to the current Minimum Spanning Tree.
+- Another array parent[] to store indexes of parent nodes in MST. The parent array is the output array, which is used to show the constructed MST.
 */
 
 // Prim's Algorithm [TC: O(V*V)]
@@ -40,14 +40,14 @@ int primMST(int graph[V][V])
     int res = 0;
 
     vector<bool> mSet(V, false); // To keep track the of vertices included in MST.
-    key[0] = 0;
+    key[0] = 0;                  // starting vertex
 
-    // The MST will have V vertices
+    // The MST will have V vertices. Hence, we iterate V times to add V-1 edges to the MST.
     for (int count = 0; count < V; count++)
     {
-        int u = -1;
+        int u = -1; // to store the minimum key value among the vertices not yet included in the MST
 
-        // Picking minimum key vertex from the set of vertices not yet included in MST (NOTE: In first iteration we will always pick vertex)
+        // Picking minimum key vertex from the set of vertices not yet included in MST. (NOTE: In first iteration we will always pick vertex)
         for (int i = 0; i < V; i++)
         {
             if (!mSet[i] && (u == -1 || key[i] < key[u]))
@@ -59,12 +59,18 @@ int primMST(int graph[V][V])
         mSet[u] = true; // Mark it as Included
         res += key[u];  // Add Key value to result
 
+        /*
+        As new node is already added to tree => We update the key array.
+        Iterate over all vertices v that are not yet included in the MST and update the key value of v if the weight of the
+        edge (graph[u][v]) is smaller than the current key value of v.
+        */
         for (int v = 0; v < V; v++)
         {
-            // Updating key value of adjacent vertices which are not yet visited
+            // graph[u][v] != 0 => edge exists between u and v. mSet[v] = false => The vertex is not included in mst set
             if (graph[u][v] != 0 && mSet[v] == false)
             {
-                key[v] = min(key[v], graph[u][v]); // To get minimum edge value
+                // Updates key values for next iteration. The minimum key will be picked in next iteration
+                key[v] = min(key[v], graph[u][v]);
             }
         }
     }
